28a6e72b121cb4b8543563b2fd73c2626d172891
Eleonora <2022cherepanova.ed@student.letovo.ru>
2021-04-24 17:35:47 +0300

    v3 db tests on multiple operations added

--- app2.py
+++ app2.py
@@ -1,9 +1,12 @@
-from flask import Flask, jsonify, request
-from flask_restful import Resource, Api, reqparse
+from flask import Flask
+from flask_jwt import JWT, jwt_required
+from flask_restful import reqparse, Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SECRET_KEY'] = 'super-secret'
 
+# List of items on sale
 items = [
     {
         'name': 'default item name',
@@ -11,11 +14,51 @@ items = [
     }
 ]
 
+
+# User constructor
+class User(object):
+    def __init__(self, id_, username, password):
+        self.id = id_
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+
+# Users table
+users = [
+    User(1, 'user1', 'abcxyz'),
+    User(2, 'user2', 'abcdef'),
+]
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+
+# Authorization methods
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and (user.password == password):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+
+jwt = JWT(app, authenticate, identity)
+
+
+# Query parser
 parser = reqparse.RequestParser()
 
 
+# Class of items which allows to view, create, edit and delete one item
 class Item(Resource):
     # GET /items/<name>
+    # Get item by name
+    @jwt_required()
     def get(self, name):
         item = list(filter(lambda x: x['name'] == name, items))
         if item:
@@ -23,8 +66,10 @@ class Item(Resource):
         return {'message': 'item not found'}
 
     # POST /items/<name>
+    # Create new item by name
+    @jwt_required()
     def post(self, name):
-        parser.add_argument('price')
+        parser.add_argument('price', required=True)
         item = list(filter(lambda x: x['name'] == name, items))
         if item:
             return {'message': 'item already exists'}
@@ -35,8 +80,11 @@ class Item(Resource):
         return {'message': 'price is not an integer'}
 
     # PUT /items/<name>
+    # Edit price of an item by name
+    # If no item found by name, create item by name
+    @jwt_required()
     def put(self, name):
-        parser.add_argument('price')
+        parser.add_argument('price', required=True)
         item = list(filter(lambda x: x['name'] == name, items))
         price = parser.parse_args()['price']
         if item:
@@ -47,6 +95,8 @@ class Item(Resource):
         Item.post(self, name)
 
     # DELETE /items/<name>
+    # Delete item by name
+    @jwt_required()
     def delete(self, name):
         item = list(filter(lambda x: x['name'] == name, items))
         if item:
@@ -55,17 +105,19 @@ class Item(Resource):
         return {'message': 'item not found'}
 
 
-api.add_resource(Item, '/items/<name>')
-
-
 class ItemList(Resource):
     # GET /items
+    # Get a list of all items
+    @jwt_required()
     def get(self):
         return {'items': items}
 
     # POST /items
+    # Create several items
+    # Takes dict with key 'items' and value list of item dicts
+    @jwt_required()
     def post(self):
-        parser.add_argument('itemlist', type=dict, action='append')
+        parser.add_argument('itemlist', type=dict, action='append', required=True)
         itemlist = parser.parse_args()['itemlist']
         messages = []
         for item in itemlist:
@@ -79,6 +131,8 @@ class ItemList(Resource):
         return {'messages': messages, 'items': items}
 
 
+# Endpoints
+api.add_resource(Item, '/items/<name>')
 api.add_resource(ItemList, '/items')
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -10,12 +10,24 @@ create_user = 'INSERT INTO users VALUES (?, ?, ?)'
 user = (None, 'elya_cherry', '1234')
 cur.execute(create_user, user)
 
+users = [
+        ('dima', 'slave'),
+        ('timofey', 'rab')
+    ]
+cur.executemany(create_user, users)
+
 create_items_table = 'CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price integer)'
 cur.execute(create_items_table)
 
-create_item = 'INSERT INTO items VALUES (?, ?, ?)'
+create_item = 'INSERT INTO items(id, name, price) VALUES (?, ?, ?)'
 item = (None, 'chair', '1221')
 cur.execute(create_item, item)
+
+items = [
+        ('bread', 50),
+        ('cheese', 200)
+    ]
+cur.executemany(create_item, items)
 
 con.commit()
 con.close()
