b3956e2d4785150817a097d9fab72d3b20cde849
Eleonora <2022cherepanova.ed@student.letovo.ru>
2021-05-22 14:06:20 +0300

    another version of models.users that might work

--- restful_v4/resources/users.py
+++ restful_v4/resources/users.py
@@ -1,5 +1,5 @@
-from flask_restful import Resource, reqparse
-
+from flask_restful import Resource, reqparse, abort
+from flask_jwt import jwt_required
 from models.users import UserModel
 
 
@@ -13,17 +13,18 @@ class UserRegisterResource(Resource):
     @staticmethod
     def post():
         user = UserRegisterResource.parser.parse_args()
-        if UserModel.find_by_username(user['username']):
-            return {'message': f"user {user['username']} already exists"}, 400
-        new_user = UserModel(username=user['username'], password=user['password'])
+        if UserModel.find_by_username(user.username):
+            abort(409, message="User Already Exists")
+        new_user = UserModel(**user)
         new_user.add_user()
-        return {'message': f"added user '{user['username']}'"}, 201
+        return {'message': f"added {user.username}"}, 201
 
-    @staticmethod
-    def delete():
+    @jwt_required()
+    def delete(self):
         user = UserRegisterResource.parser.parse_args()
-        if UserModel.find_by_username(user['username']):
-            user = UserModel.query.filter_by(username=user['username'])
+        if UserModel.find_by_username(user.username):
+            user = UserModel.query.filter_by(username=user.username)
             user.delete_user()
-            return {'message': f"user '{user}' was deleted successfully"}, 204
-        return {'message': f"user '{user}' does not exist"}, 404
+            return {'message': f"user removed"}, 204
+
+        return {'message': f"user not found"}, 404
